c5c2bf1247a53ac74f5ff7657792636940203d1d
nawarajjaishi <rajanrajjaishi288@gmail.com>
2017-12-21 09:55:02 +0545

    These all are practices of java project class

---
+++ src/String/OOPJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package String;
+
+/**
+ *
+ * @author Jaction76
+ */
+public class OOPJava {
+
+    public static void main(String[] args) {
+        String_Comparision_Methods scm = new String_Comparision_Methods();
+        scm.equals_method();
+        scm.ignorecase();
+        scm.by_equals_operator();
+        scm.compare_to_method();
+    }
+
+}
